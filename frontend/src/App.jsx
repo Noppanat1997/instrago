f8dc5badcc7440d13924e59b18b29170f820fb99
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-10-12 15:10:19 +0700

    change from js to jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./component/navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TodoPage from "./container/todoPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/todo" component={Navbar} />
+        <Route path="/todo" component={TodoPage} />
         <Navbar />
       </Switch>
     </Router>
---
+++ frontend/src/container/todoPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../component/navbar"
+
+const TodoPage = () => {
+  return (
+    <div className="TodoPage">
+      <NavBar />
+      <h1>sdfsdf</h1>
+    </div>
+  );
+};
+
+export default TodoPage;
