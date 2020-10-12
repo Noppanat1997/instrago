ee036fd29d943348c2081bc075cff678a17437d5
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-10-12 18:04:56 +0700

    NavBar now have state color

--- frontend/src/component/navbar/index.jsx
+++ frontend/src/component/navbar/index.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [mode, setMode] = useState(0)
+
+  useState(() => {
+    setMode(window.location.pathname)
+  }, [])
+
   return (
     <div className="navbar">
       <div className="content">
         <div className="brand">TODONA</div>
         <div className="link-page">
-          <Link to="/">Todo</Link>
-          <Link to="/done">Done</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/" onClick={() => setMode(0)} style={mode === "/" ? { color: "#009BFF" } : null}>Todo</Link>
+          <Link to="/done" onClick={() => setMode(1)} style={mode === "/done" ? { color: "#009BFF" } : null}>Done</Link>
+          <Link to="/contact" onClick={() => setMode(2)} style={mode === "/contact" ? { color: "#009BFF" } : null}>Contact</Link>
         </div>
         <button className="create-button">New Task</button>
       </div>
